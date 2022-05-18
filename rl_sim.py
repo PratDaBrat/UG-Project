5ba3e074c79b2415188fcf6c68c6a946281fb8a0
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-18 14:13:42 +0530

    rl base

---
+++ rl_sim.py
@@ -0,0 +1,18 @@
+from maze import *
+import time
+import random
+
+X,Y = 20, 20    #random.choice(range(50,100)),random.choice(range(50,100))
+W = 0.1         #random.random() * 10000 // 100 / 100 #0.1 #sparseness
+food = 2
+
+#maze generation
+m = maze(X,Y,W,food).generate()
+plane = m.plane
+walls = m.walls
+s = m.s
+e = m.e
+final = m.final
+path = m.path
+
+visited = [s[0]]
