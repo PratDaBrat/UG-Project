031cfdb1359b3bafb5f0be2a5f77b649fd06cc0b
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-19 14:26:34 +0530

    changed colors

--- maze.py
+++ maze.py
@@ -59,11 +59,11 @@ class maze():
 			print()
 
 	def graphdisp(self):
-		color_map = {0: np.array([200,200,200]), 
-		1: np.array([0, 0, 0]), 
-		2: np.array([142, 245, 39]), 
-		3: np.array([245, 56, 39]), 
-		4: np.array([100, 100, 100])}
+		color_map = {0: np.array([54, 0, 0]), 
+		1: np.array([255, 0, 0]), 
+		2: np.array([2, 173, 5]), 
+		3: np.array([2, 110, 173]), 
+		4: np.array([255, 255, 255])}
 		
 		self.display = [[color_map[0] for i in range(self.X)] for i in range (self.Y)]
 		
@@ -93,6 +93,6 @@ class maze():
 		plt.savefig('test.png')
 		return img
 
-m = maze(10,10,0.1,1).generate()
+m = maze(100,100,0.05,300).generate()
 # m.disp()
 m.graphdisp()
