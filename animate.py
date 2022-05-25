3de3b14e642058aff68bc286c4119afe6eb6e2a3
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-25 22:16:18 +0530

    animation script to animate agent movement from stateimages

---
+++ animate.py
@@ -0,0 +1,13 @@
+import cv2
+
+def makeVideo():
+	fourcc  = cv2.VideoWriter_fourcc('M','J','P','G')
+	out = cv2.VideoWriter('animation.mp4', fourcc, 1.0, (640,480))
+	for i in range(4):
+		img_path = f'stateimages/{i}.png'
+		print(img_path)
+		frame = cv2.imread(img_path)
+		out.write(frame)
+	out.release
+
+makeVideo()
