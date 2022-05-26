0bf078d518111dd7867666660bc65093dcaf5046
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-26 22:25:18 +0530

    movement based on action space check ok

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from Maze import *
 import random
 
-X,Y,W,food = 10,10,0.5,1
+X,Y,W,food = 10,10,0.1,1
 
 m = Maze(X,Y,W,food).generate()
 a = m.s[0]
 travel = 0
 done = False
 i = 0
-while i <= 10:
+while i <= 20:
 	# m.disp()
 	print(m.getActionSpace())
 	m.graphDisp(f'stateimages/{i}.png')
