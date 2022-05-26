3d99d2b47ae84ef5b82c33900ae34e5602c1209c
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-26 21:54:57 +0530

    travelPenalty-s implementation check ok

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from Maze import *
 
-X,Y,W,food = 10,10,0,1
+X,Y,W,food = 10,10,0.5,1
 
 m = Maze(X,Y,W,food).generate()
 a = m.s[0]
+travel = 0
 for i in [0,1,2,3]:
-	m.graphDisp(f'stateimages/{i}.png')
+	# m.graphDisp(f'stateimages/{i}.png')
 	a.action(i)
-	m.updateAgent(a)
+	travel += m.updateAgent(a)
+print(travel)
