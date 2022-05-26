2b16dfaaa429dba78a1278c7518dbfce69d2fa2f
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-26 21:30:18 +0530

    animation script

--- animate.py
+++ animate.py
@@ -3,7 +3,7 @@ import cv2
 def makeVideo():
 	fourcc  = cv2.VideoWriter_fourcc('M','J','P','G')
 	out = cv2.VideoWriter('animation.mp4', fourcc, 1.0, (640,480))
-	for i in range(4):
+	for i in range(4): #no.of images
 		img_path = f'stateimages/{i}.png'
 		print(img_path)
 		frame = cv2.imread(img_path)
