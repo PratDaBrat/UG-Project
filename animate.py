8a89bfa61ab6a7129c6c48106d207892f48809f6
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-30 21:57:32 +0530

    testing reset + new animate implementation

--- animate.py
+++ animate.py
@@ -1,13 +1,11 @@
 import cv2
 
-def makeVideo():
+def makeVideo(z,n,name='animation.mp4'):
 	fourcc  = cv2.VideoWriter_fourcc('M','J','P','G')
-	out = cv2.VideoWriter('animation.mp4', fourcc, 1.0, (640,480))
-	for i in range(50): #no.of images
+	out = cv2.VideoWriter(name, fourcc, 1.0, (640,480))
+	for i in range(z,n): #no.of images
 		img_path = f'stateimages/{i}.png'
 		print(img_path)
 		frame = cv2.imread(img_path)
 		out.write(frame)
 	out.release
-
-makeVideo()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from Maze import *
 import random
+from animate import makeVideo
 
 X,Y,W,FOOD = 30,30,0.1,1
 
@@ -11,7 +12,7 @@ E = M.e
 FINAL = M.final
 PATH = M.path
 A = M.s[0]
-RESET = 5
+RESET = 10
 travel = []
 done = False
 i = 0
@@ -19,10 +20,18 @@ while i <= 50:
 	# m.disp()
 	# print(m.getActionSpace())
 	M.graphDisp(f'stateimages/{i}.png')
-	A.action(random.choice([1,2]))
+	if A.x <= 15 and A.y <= 15:
+		A.action(random.choice([0,1]))
+	elif A.x > 15 and A.y > 15:
+		A.action(random.choice([2,3]))
+	elif A.x <= 15 and A.y > 15:
+		A.action(random.choice([3,1]))
+	if A.x > 15 and A.y <= 15:
+		A.action(random.choice([0,2]))
 	travel.append(M.updateAgent(A))
 	i += 1
 	if i % RESET == 0:
 		M.reset()
+		makeVideo((i//RESET - 1)*RESET,i,f'animation{i}.mp4')
 print(travel)
 print(sum(travel))
