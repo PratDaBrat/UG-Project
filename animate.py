83ac6b78b7256a1e580f5e5fe87ce3474f788f21
PratDaBrat <pratyush.chow8@gmail.com>
2022-05-30 23:47:55 +0530

    rl test 1

--- animate.py
+++ animate.py
@@ -4,7 +4,7 @@ def makeVideo(z,n,episode,name='animation.mp4'):
 	fourcc  = cv2.VideoWriter_fourcc('M','J','P','G')
 	out = cv2.VideoWriter(name, fourcc, 1.0, (640,480))
 	for i in range(z,n): #no.of images
-		img_path = f'stateimages/{episode}_{i}.png.png'
+		img_path = f'stateimages/{episode}_{i}.png'
 		print(img_path)
 		frame = cv2.imread(img_path)
 		out.write(frame)
