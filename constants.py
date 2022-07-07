91aae5c18a1eea463dadf5b77b6a7115770ef191
PratDaBrat <pratyush.chow8@gmail.com>
2022-07-07 18:23:31 +0530

    updated costs

--- constants.py
+++ constants.py
@@ -1,18 +1,18 @@
-X,Y = 50, 50    #random.choice(range(50,100)),random.choice(range(50,100))
+X,Y = 10, 10    #random.choice(range(50,100)),random.choice(range(50,100))
 W = 0.1        #random.random() * 10000 // 100 / 100 #0.1 #sparseness
-FOOD = 50
+FOOD = 5
 
-EPISODES = 6000
+EPISODES = 1000
 MOVE_PENALTY = -1
-ENEMY_PENALTY = -20
+ENEMY_PENALTY = -100
 STAT_PENALTY = -5
-FOOD_REWARD = 5
+FOOD_REWARD = 10
 
 epsilon = 0.85
 EPS_DECAY = 0.998
 
-SHOW_EVERY = 150
-MAX_STEPS = 250
+SHOW_EVERY = 100
+MAX_STEPS = 50
 
-# LEARNING_RATE = 0.2
+LEARNING_RATE = 0.2
 DISCOUNT = 0.95
