783a0f8ee0944e009bde28deb7fcfa94942f0943
PratDaBrat <pratyush.chow8@gmail.com>
2023-05-09 12:14:20 +0530

    updates

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+# import seaborn as sns
 
-file = open('times75x75.txt','r')
+file = open('times100x100.txt','r')
 solved = []
 unsolved = []
 
-N = 506 #1800
+N = 1800
 m = [0 for _ in range(N)]
 
 for i in range(N):
@@ -15,8 +16,11 @@ s, t = [], []
 us, ut = [], []
 # ax = [i for i in range(N)]
 
+#{X}x{Y} {solve} {t} seconds {W} sparsity {path_len} path
+#   0       1     2     3     4     5         6       7
+
 for entry in m:
-	if entry[1] == 'solved' and int(entry[6]) > 7:
+	if entry[1] == 'solved' and int(entry[6]) > 5:
 		s += [float(entry[4])]
 		t += [float(entry[2]) if 'e' not in entry[2] else 0]
 	if entry[1] == 'unsolvable':
