2b50ebbacf89ae4af4d6fb3dd622a68fb75f2b9c
Pratyush Chowdhury <pratyush.chow8@gmail.com>
2024-05-04 16:46:56 +0530

    PEP-8 compliance

--- Agent.py
+++ Agent.py
@@ -14,30 +14,30 @@ class Agent:
 	def __str__(self):
 		return f"{self.x}, {self.y}"
 
-	def __lt__(self,other):
+	def __lt__(self, other):
 		return self.cost < other.cost
 
-	def action(self,i):
+	def action(self, i):
 		if i == 0:
-			self.move(1,0)  #R
+			self.move(1, 0)   #R
 		elif i == 1:
-			self.move(0,1) #D
+			self.move(0, 1)   #D
 		elif i == 2:
-			self.move(-1,0) #L
+			self.move(-1, 0)  #L
 		elif i == 3:
-			self.move(0,-1)  #U
+			self.move(0, -1)  #U
 		else:
 			self.move()
 
 	def move(self, x=0, y=0):
 		if self.previous is None:
-			self.previous = Agent(self.x,self.y)
+			self.previous = Agent(self.x, self.y)
 		if x != 0 or y != 0:
 			self.x += x
 			self.y += y
 		else:
-			print('random move')
+			# print('random move')
 			if random.random() * 10000 // 100 % 2:
-				self.x += random.choice([-1,1])
+				self.x += random.choice([-1, 1])
 			else:
-				self.y += random.choice([-1,1])
+				self.y += random.choice([-1, 1])
